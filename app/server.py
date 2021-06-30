d73e5da47e3068b9daeb6dd36c032e4e118148d6
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-06-30 12:32:31 +0200

    remove download and refactor parameter names name --> filename

--- app/server.py
+++ app/server.py
@@ -1,21 +1,21 @@
-from flask import json, jsonify, Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from flask import jsonify, Flask, render_template, flash, request, redirect, url_for
 from flask_cors import CORS
 
 import logging
 import argparse
 import os, sys
 from werkzeug.utils import secure_filename
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, InternalServerError
 from PDFReader import pdf2text
 
-UPLOAD_FOLDER = './.upload/'
+UPLOAD_FOLDER = './.upload'
 ALLOWED_EXTENSIONS = {'pdf', 'txt'}
 
 app = Flask(__name__)
 CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app.add_url_rule("/download/<name>", endpoint="download_file", build_only=True)
+
 app.add_url_rule("/info/<name>", endpoint="info_file", build_only=True)
 app.add_url_rule("/upload", endpoint="upload", build_only=True)
 
@@ -30,6 +30,7 @@ def try_save_file_input(files):
     
     file = files['file']
 
+    # Check Input
     if file.filename == '':
         flash('No selected file')
         raise BadRequest("Invalid Input - no selected file - filename is empty")
@@ -38,8 +39,17 @@ def try_save_file_input(files):
         flash('Format not supported')
         raise BadRequest(f"Invalid Input - format is not supported - supported only: {ALLOWED_EXTENSIONS}")
 
+    # Save Input
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    file_path = f"{app.config['UPLOAD_FOLDER']}/{filename}"
+
+    try:
+        assert os.path.isdir(app.config['UPLOAD_FOLDER'])
+        file.save(file_path)
+        assert os.path.isfile(file_path), "File has not been saved!"
+    except Exception as e:
+        raise InternalServerError(f"Unable to store pdf! {e}")
+
     return filename
 
 
@@ -48,29 +58,24 @@ def home():
     return redirect(url_for('upload'))
 
 
-
 @app.route('/api/text', methods=['POST'])
 def text_extraction():
     filename = try_save_file_input(request.files)
-    text = pdf2text(f"{app.config['UPLOAD_FOLDER']}{filename}")
-    return jsonify({'text': text, 'id': filename, 'url': url_for('download_file', name=filename), 'html_body': render_template('file_upload_info.html', filename=filename, text=text)})
+    text = pdf2text(f"{app.config['UPLOAD_FOLDER']}/{filename}")
+    return jsonify({'text': text, 'id': filename, 'url': url_for('info_file', filename=filename), 'html_body': render_template('file_upload_info.html', filename=filename, text=text)})
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         filename = try_save_file_input(request.files)
-        return redirect(url_for('info_file', name=filename))
+        return redirect(url_for('info_file', filename=filename))
     
     return render_template('file_upload.html')
 
-@app.route('/info/<name>')
-def info_file(name):
-    text = pdf2text(f"{app.config['UPLOAD_FOLDER']}{name}")
-    return render_template('file_upload_info.html', filename=name, text=text)
-
-@app.route('/download/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route('/info/<filename>')
+def info_file(filename):
+    text = pdf2text(f"{app.config['UPLOAD_FOLDER']}/{filename}")
+    return render_template('file_upload_info.html', filename=filename, text=text)
 
 if __name__ == '__main__':
 
